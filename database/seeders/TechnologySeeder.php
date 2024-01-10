d5cebb78bc99c6d7348f057cbe2c0953245445a8
NicolaSforza93 <nicola.sforza93@gmail.com>
2024-01-10 15:34:20 +0100

    ADD - migration and seeder of table technologies

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,14 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = ['Css', 'Javascript', 'Vue', 'Laravel', 'Php', 'Sass', 'Bootstrap', 'MySql', 'Node'];
+
+        foreach ($technologies as $technology_name) {
+            $new_technology = new Technology();
+
+            $new_technology->name = $technology_name;
+
+            $new_technology->save();
+        }
     }
 }
